a5a942fd128cafca08fcd566b724a6e97371e127
jennjunod <hello@teachjenntech.com>
2023-01-21 13:43:12 -0700

    blog

---
+++ config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Teach Jenn Tech';
+export const SITE_DESCRIPTION = 'Teach Jenn Tech creates a space to allow individuals to remove shame, explore tech, and share resources with thought leaders in the industry.';
