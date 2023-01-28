480bcdc70efa563a1425e8e171c376a31fed7810
jennjunod <hello@teachjenntech.com>
2023-01-27 18:25:01 -0700

    stream schedule

---
+++ src/data/paststream/index.ts
@@ -0,0 +1,37 @@
+// adapted from https://github.com/withastro/astro.build/blob/112bdc723b3ba305997c95d7ce02304624d0d3ce/src/data/showcase/index.ts
+
+import type { StreamSite } from "~/types";
+import sitesData from "./paststream.json"
+
+const allImages = import.meta.glob<ImageMetadata>("./images/*.{png,jpg,jpeg}", {
+  eager: true,
+  import: "default",
+});
+
+let _loadShowcase: Promise<Array<StreamSite>>;
+
+async function loadShowcase(): Promise<Array<StreamSite>> {
+  const sites = await Promise.all(
+    sitesData.map(async (site) => {
+      if (!(site.image in allImages)) {
+        console.error(
+          `Image for "${site.title}" not found (provided: "${site.image}")`
+        );
+      }
+
+      const image = await allImages[site.image];
+
+      return {
+        ...site,
+        image,
+      };
+    })
+  );
+
+  return sites;
+}
+
+export async function getPastStream() {
+  _loadShowcase = _loadShowcase || loadShowcase();
+  return _loadShowcase;
+}
